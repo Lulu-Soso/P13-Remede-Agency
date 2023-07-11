9a7baf21e875c9f701ba2a92191aadb497e651e0
Lulu-Soso <lucas.sokha@gmail.com>
2023-07-11 11:48:31 +0200

    create connexion with MongoDB

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+  // process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+  process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/argentBankDB'
 
 module.exports = async () => {
   try {
     // await mongoose.connect(databaseUrl, { useNewUrlParser: true})
     await mongoose.connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-    console.log('Database successfully connected')
+    console.log('Database successfully connected, Mongo connected !')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
     throw new Error(error)
